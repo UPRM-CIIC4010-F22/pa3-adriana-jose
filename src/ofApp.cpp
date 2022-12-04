d4e31220e5444a7e3ad3fb555584debd5ceb707f
Adriana-Isabel <adriana.carrasquillo1@upr.edu>
2022-12-03 21:01:26 -0400

    Added baby trees

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -27,6 +27,11 @@ void ofApp::draw() {
         // Tree
         float length = 0.31 * ofGetHeight();
         drawMode2(ofGetWidth() / 2, ofGetHeight() - 20, 10, length, 1.5 * PI);
+
+        drawMode2(ofGetWidth() / 4 , ofGetHeight() - 20 , 10, (length)/2, 1.5 * PI); //left baby tree
+
+        drawMode2(ofGetWidth() - 350 , ofGetHeight() - 20 , 10, (length)/2, 1.5 * PI); // right baby tree
+        
     } break;
     case '3': {
         // Sierpinski Triangle
