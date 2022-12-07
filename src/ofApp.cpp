d0927c84f2665d42f43d07c6c38a29852efd6d76
Adriana-Isabel <adriana.carrasquillo1@upr.edu>
2022-12-07 12:54:47 -0400

    Recursive Sierpinski Complete

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -108,6 +108,7 @@ void ofApp::drawMode3(float x, float y, float size, int n) {
 
     drawMode3(x, y, size / 2, n - 1);
     drawMode3((a.x + b.x) / 2, (a.y + b.y) / 2, size / 2, n - 1);
+    drawMode3((c.x + a.x) / 2, (c.y + a.y) / 2, size / 2, n - 1); // added the recursion that completed the triangle
 }
 
 void ofApp::drawMode4(float x, float y, float n) {
